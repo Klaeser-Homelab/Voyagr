76fddb4d0fc462fcfe55bc1e4fcbc62182862e95
Reed Klaeser <reed.klaeser@gmail.com>
2025-03-24 22:30:57 -0700

    Configure dev to point to production database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/manageme', {
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME || 'manageme',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
   dialect: 'postgres',
   logging: false, // Set to console.log to see SQL queries
 });
