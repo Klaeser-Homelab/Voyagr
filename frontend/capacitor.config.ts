b17011010e12a3c499a063e55ec82bfb831d4934
Reed Klaeser <reed.klaeser@gmail.com>
2025-04-28 12:07:47 -0700

    init capacitor 2

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.voyagr.app',
+  appName: 'voyagr',
+  webDir: 'dist'
+};
+
+export default config;
